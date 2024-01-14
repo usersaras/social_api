80ead580313ac00b1dcd482f8c6d1b658781be1d
usersaras <saras.sthapitshrestha@gmail.com>
2024-01-14 11:47:55 +0545

    feat: Setup cors policy

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from . import models
-from .database import engine
 from .routers import authentication, posts, users, likes
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["https://www.google.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST ...
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
